b80634fda773895daa37b9c6833344844dcbf7f3
Isaac Ramos Magalhães <160159585+IsaacMaf@users.noreply.github.com>
2024-04-19 12:43:44 -0300

    Add files via upload

---
+++ CAP 6/Exercícios Resolvidos/CAP6QR25/src/br/edu/principal/Principal.java
@@ -0,0 +1,40 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int primos[] = new int[10];
+		int i, qtde = 0, num = 101, divisores;
+		
+		System.out.println("Este programa armazena os dez primeiros numeros primos acima de 100 e os mostra.\n");
+		do {
+			divisores = 0;
+			for (i = 0; i < num; i++) {
+			
+				if (num % (i+1) == 0) {
+					
+					divisores++;
+				}
+				
+			}
+				if (divisores <= 2) {
+					
+					primos[qtde] = num;
+					qtde++;
+				}
+				
+				num++;
+			
+		}while (qtde <10);
+		
+		for (i = 0; i < 10; i++) {
+			
+			System.out.println("O " + (i+1) + "° numero primo depois de 100 e: " + primos[i]);
+		}
+		
+		System.out.println("\nFim de programa.");
+		
+	}
+
+}
